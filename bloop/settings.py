c31b19ab90cdc7e6bf5a0e8c5643db131df99336
andyyc <andy.y.c@gmail.com>
2015-06-15 11:01:53 -0700

    Update settings.py

--- bloop/settings.py
+++ bloop/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dev_db',                      # Or path to database file if using sqlite3.
-        'USER': 'fthd',                      # Not used with sqlite3.
-        'PASSWORD': 'asdfawef',                  # Not used with sqlite3.
+        'USER': 'asdf',                      # Not used with sqlite3.
+        'PASSWORD': 'asdfasdf',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -162,8 +162,8 @@ LOGGING = {
 LOGIN_URL = '/login'
 
 #Facebook Login
-FACEBOOK_APP_ID = '205625196237126'
-FACEBOOK_API_SECRET = 'c055fe62c6d968f0f1e452baeef26539'
+FACEBOOK_APP_ID = '1111'
+FACEBOOK_API_SECRET = 'asdf'
 FACEBOOK_REDIRECT_URI = 'http://localhost:8000/login/'
 
 AUTHENTICATION_BACKENDS = (
